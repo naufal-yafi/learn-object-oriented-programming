5f2f88c7b74aea70bbeead305a19d77a4303286b
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-03-06 20:57:50 +0700

    split testing

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,8 +1,8 @@
 import EmptyClass from "../src/1_class";
 import Customer from "../src/2_contructor";
+import People from "../src/3_properties";
 import DefaultValue from "../src/4_default-value";
 import Method from "../src/5_method";
-import People from "../src/3_properties";
 
 describe("Basic", () => {
   it("Intance new empty class", () => {
@@ -14,31 +14,40 @@ describe("Basic", () => {
     new Customer();
   });
 
-  it("Properties", () => {
+  describe("Properties", () => {
     const people1 = new People(1, "Naufal");
     const people2 = new People(2, "Yafi");
 
-    expect(people1.id).toBe(1);
-    expect(people1.name).toBe("Naufal");
-    expect(people1.age).toBe(undefined);
+    it("Checking content", () => {
+      expect(people1.id).toBe(1);
+      expect(people1.name).toBe("Naufal");
+      expect(people1.age).toBe(undefined);
 
-    expect(people2.id).toBe(2);
-    expect(people2.name).toBe("Yafi");
-    expect(people2.age).toBe(undefined);
+      expect(people2.id).toBe(2);
+      expect(people2.name).toBe("Yafi");
+      expect(people2.age).toBe(undefined);
+    });
 
-    people1.age = 20;
-    expect(people1.age).toBe(20);
+    it("Field age", () => {
+      people1.age = 20;
+      expect(people1.age).toBe(20);
 
-    people2.age = 80;
-    expect(people2.age).toBe(80);
+      people2.age = 80;
+      expect(people2.age).toBe(80);
+    });
   });
 
-  it("Default Value", () => {
+  describe("Default Value", () => {
     const typeName = new DefaultValue(1);
 
-    expect(typeName.name).toBe("Type your name");
-    typeName.name = "Naufal";
-    expect(typeName.name).toBe("Naufal");
+    it("Value default content", () => {
+      expect(typeName.name).toBe("Type your name");
+    });
+
+    it("Field name", () => {
+      typeName.name = "Naufal";
+      expect(typeName.name).toBe("Naufal");
+    });
   });
 
   it("Method", () => {
